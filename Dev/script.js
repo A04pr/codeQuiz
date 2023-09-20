a8f14d4069bd1b563dce88e34507adce313a7901
Aidan Reed <areed041704@gmail.com>
2023-09-19 23:36:22 -0400

    some js work

--- Dev/script.js
+++ Dev/script.js
@@ -1,1 +1,71 @@
-var timeLeft = document.querySelector('.timeLeft')
+var timeLeft = $('.timeLeft')
+var quiz = $("#questions")
+var score = $('.score')
+var finalScore = $('#finalScore')
+var titleScreen = $(".titleScreen")
+var startButton = $('.startButton')
+var goBack = $('.goBackButton')
+var clear = $('.clearHighscoreButton')
+var scoreCount = 0;
+var count = 25;
+var timer;
+
+var quizQuestions = [
+    {
+        id: "q1",
+        question: "",
+        answers: [
+            "a",
+            "b",
+            "c",
+            "d",
+        ],
+        correct: "a",
+    },
+    {
+        id: "q2",
+        question: "",
+        answers: [
+            "a",
+            "b",
+            "c",
+            "d",
+        ],
+        correct: "c",
+    },
+    {
+        id: "q3",
+        question: "",
+        answers: [
+            "a",
+            "b",
+            "c",
+            "d",
+        ],
+        correct: "c",
+    },
+    {
+        id: "q4",
+        question: "",
+        answers: [
+            "a",
+            "b",
+            "c",
+            "d",
+        ],
+        correct: "d",
+    },
+    {
+        id: "q5",
+        question: "",
+        answers: [
+            "a",
+            "b",
+            "c",
+            "d",
+        ],
+        correct: "b",
+    },
+];
+
+
